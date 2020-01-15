d3c112ad3aefb33246cd9244bbee638f039fd3af
devanyeawilliams <devanyea.williams@ada.ac.uk>
2020-01-15 14:05:53 +0000

    task 2 done

--- models/person.js
+++ models/person.js
@@ -1,1 +1,42 @@
 // Your Person class goes here!
+class Person{
+    constructor(firstName, lastName, dob){
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
+        this.lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1)
+        this.dob = dob
+        this.emails = []
+        this.mobile = []
+    }
+    fullname(){
+        return this.firstName + " " + this.lastName
+    }
+    addEmail(email){
+        this.emails.push(email)
+    }
+    addMobile(mobile){
+        this.mobile.push(mobile)
+    }
+    formatString(){
+        var string = this.firstName +" "+ this.lastName + "\n" + "----------" + "\n" + "DOB: " + this.dob + "\n" + "\n" + "Email Addresses:" + "\n"+ "\n";
+        let emailcounter = 0;
+        while (emailcounter <= this.emails.length - 1){
+            string = string + "- " + this.emails[emailcounter] + "\n";
+            emailcounter += 1
+        }
+        let mobileCounter = 0;
+        string = string + "\n" + "Phone Numbers:" + "\n";
+        while (mobileCounter <= this.mobile.length - 1){
+            string = string+ "- " + this.mobile[mobileCounter]+ "\n";
+            mobileCounter += 1
+        }
+        var newStr = string.slice(0, -1);
+        return newStr
+
+
+    }
+
+
+
+
+}
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,58 @@
-const chai = require('chai')
-const expect = chai.expect
+const chai = require('chai');
+const expect = chai.expect;
 
+const Person = require("../models/person.js");
 // Your Person test goes here!
+
+describe('person', ()=> {
+    it('should initialize properly', ()=>{
+        var person = new Person("Joe","Bloggs", "1 Jan 1998");
+        expect(person.firstName).to.equal("Joe");
+        expect(person.lastName).to.equal("Bloggs");
+        expect(person.dob).to.equal("1 Jan 1998");
+        expect(person.emails).to.deep.equal([])
+    });
+    it('should capitalise the first name', ()=>{
+        var person = new Person("ben","Dover", "1 Jan 1998");
+        expect(person.firstName).to.equal("Ben")
+    });
+
+    it('should return the full name', ()=>{
+        var person = new Person("ben","dover", "1 Jan 1998");
+        expect(person.fullname()).to.equal("Ben Dover")
+    });
+
+    it('should add email', ()=>{
+        var person = new Person("ben","dover", "1 Jan 1998");
+        var email = 'ben3@gmail.com';
+        person.addEmail(email);
+        expect(person.emails).to.deep.equal(['ben3@gmail.com'])
+    });
+
+    it('should add mobile', ()=>{
+        var person = new Person("ben","dover", "1 Jan 1998")
+        var mobile = '07342040402';
+        person.addMobile(mobile);
+        expect(person.mobile).to.deep.equal(['07342040402'])
+    })
+
+    it('should return format string', ()=>{
+        var person = new Person("Joe","bloggs", "1 Jan 1990");
+        person.addEmail("joe@example.com");
+        person.addEmail("joe.bloggs@workexample.com");
+        person.addMobile("07712345678");
+        person.addMobile("07654321987");
+        expect(person.formatString()).to.equal("Joe Bloggs" +"\n"+
+            "----------" +"\n"+
+            "DOB: 1 Jan 1990" + "\n"+
+            "\n" +
+            "Email Addresses:\n" + "\n"+
+            "- joe@example.com"+ "\n"+
+            "- joe.bloggs@workexample.com" + "\n"+
+            "\n"+
+        "Phone Numbers:" + "\n"+
+            "- 07712345678" + "\n"+
+            "- 07654321987")
+    })
+
+    })
